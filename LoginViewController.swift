0333f1e6d53765d15fa484acda967a5429ca3612
Zacharyg88 <zacharyg88@gmail.com>
2017-08-22 15:39:01 -0400

    Built LoginViewController in Storyboard

---
+++ LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  On The Map
+//
+//  Created by Zach Eidenberger on 8/22/17.
+//  Copyright © 2017 ZacharyG. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
